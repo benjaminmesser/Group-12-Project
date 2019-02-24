f5c92f15afce3abdb33e78396c2e4013403f7614
SagePlsStop <47367639+SagePlsStop@users.noreply.github.com>
2019-02-24 14:37:47 -0700

    Update RunGame.java
    Added initialization method which calls board and player constructors 
    Added a "menu" for player to start game

--- RunGame.java
+++ RunGame.java
@@ -1,10 +1,37 @@
+import java.util.Scanner;
+
+public class RunGame {
+	//initalizes the game by creating the board and player
+	public static void init() {
+		Board b= new Board();
+		Player p= new Player();
+		
+	}
+	//Basically just how the game is run
+	public static void main(String[] args) {
+		//Opens "start menu" that allows player to start game
+		System.out.println("Welcome to our adventure game");
+		System.out.println("Please enter 1 to start");
+		Scanner startIn= new Scanner(System.in);
+		int startNumber= startIn.nextInt();
+		if(startNumber==1) {
+			init();
+		}
+		startIn.close();
+		
+	}
+
+}
+
+/**
 public class RunGame(){
-  // basically just how the game is run
-  public static void main(String[] args){
-    Board b = new Board();
-    while (true){
-      // essentially: keeps checking for input, calls movement methods (b.moveLeft(), b.moveRight(), etc.) accordingly...
-      // basically manages all user interaction stuff here.
-    }
+// basically just how the game is run
+public static void main(String[] args){
+  Board b = new Board();
+  while (true){
+    // essentially: keeps checking for input, calls movement methods (b.moveLeft(), b.moveRight(), etc.) accordingly...
+    // basically manages all user interaction stuff here.
   }
 }
+}
+**/

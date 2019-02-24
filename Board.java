bc007f819424c3de5402cff297ba5a427824bcc2
SagePlsStop <47367639+SagePlsStop@users.noreply.github.com>
2019-02-24 14:40:30 -0700

    Update Board.java
    Created a board of Arraylists within ArrayLists that has the demensions of 15x5 
    Rows are staggered starting at the second line and I don't know how to fix it if anyone wants to try please do!!!

--- Board.java
+++ Board.java
@@ -1,16 +1,37 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Board{
   private Player p;
-  private char[][] map;
+  private ArrayList<ArrayList<String>> map;
   public Board(){
     this.p = new Player();
     this.map = createMap();
   }
-  public char[][] createMap(){
-    // draw map here, whoever wants to do that.
+  public ArrayList<ArrayList<String>> createMap(){
+	  ArrayList<ArrayList<String>> map= new ArrayList<ArrayList<String>>();
+	  ArrayList<String> space= new ArrayList<String>();
+	  ArrayList<String> newLine= new ArrayList<String>();
+	  newLine.add("\n");
+	  //adds row to board
+	  int numOfRows= 5;
+	  //Creates a board that is numOfRows long and i wide
+	  while (numOfRows != 0) {
+	  for (int i= 0; i<=15; i++) {
+		  map.add(space);
+		  if(i == 15) {
+			  map.add(newLine);
+		  }
+	  }
+	  numOfRows-=1;
+	  }
+	  System.out.println(map);
+	  return map;
   }
   public void updateMap(){
     // adds all necessary changes to the map, for now just moving the player character. called after moves are made.
   }
+  /**
   public void moveLeft(){
     // checks to see if space is free to the left of the player's current position; if so, changes xPos accordingly.
     // Something like the following: may need to be changed accordingly if setup changes. Similar kind of idea for the other three.
@@ -33,5 +54,6 @@ public class Board{
     // checks to see if space is available below the player's current position; if so, changes yPos accordingly.
     updateMap();
   }
+  **/
   
 }

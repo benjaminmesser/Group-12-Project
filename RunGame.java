ff214b46c9ace195b0ada49b2939979db9274dd8
g-heemeryck <grace.heemeryck1@ucalgary.ca>
2019-02-24 12:18:05 -0700

    Create RunGame.java

---
+++ RunGame.java
@@ -0,0 +1,10 @@
+public class RunGame(){
+  // basically just how the game is run
+  public static void main(String[] args){
+    Board b = new Board();
+    while (true){
+      // essentially: keeps checking for input, calls movement methods (b.moveLeft(), b.moveRight(), etc.) accordingly...
+      // basically manages all user interaction stuff here.
+    }
+  }
+}

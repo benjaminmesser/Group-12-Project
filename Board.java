1768ca381bce7ec14c90d63e61eadcda0a358309
benjaminmesser <47367888+benjaminmesser@users.noreply.github.com>
2019-02-25 00:15:13 -0700

    Update Board.java
    Fixed some mistakes in moveLeft() and added moveRight(), jump(), and fall() methods.

--- Board.java
+++ Board.java
@@ -15,21 +15,36 @@ public class Board{
     // checks to see if space is free to the left of the player's current position; if so, changes xPos accordingly.
     // Something like the following: may need to be changed accordingly if setup changes. Similar kind of idea for the other three.
     if (p.getXPos() - 1 > 0){
-      if (map[p.getXPos - 1][p.getYPos].equals(' ')){
-        p.setXPos(p.getXPos - 1);
+      if (map[p.getXPos() - 1][p.getYPos()] == ' '){
+        p.setXPos(p.getXPos() - 1);
       }
     }
     updateMap();
   }
   public void moveRight(){
     // checks to see if space is free to the right of the player's current position; if so, changes xPos accordingly.
+    if (p.getXPos() + 1 <= this.map[0].length){
+      if (map[p.getXPos() + 1][p.getYPos()] == ' '){
+        p.setXPos(p.getXPos() + 1);
+      }
+    }
     updateMap();
   }
   public void jump(){
     // checks to see if space is free above the player's current position; if so, changes yPos accordingly.
+    if (p.getYPos() + 1 <= this.map[0].length){
+      if (map[p.getXPos()][p.getYPos() + 1] == ' ')
+        p.setYPos(p.getYPos() + 1);
+      }
+    }
     updateMap();
   }
   public void fall(){
+    if (p.getYPos() - 1 > 0){
+      if (map[p.getXPos()][p.getYPos() - 1] == ' ')
+        p.setYPos(p.getYPos() - 1);
+      }
+    }
     // checks to see if space is available below the player's current position; if so, changes yPos accordingly.
     updateMap();
   }

da0b7084a02ceb7e950f2ee749b85c64a1e4cc3a
Dylan Wheeler <44617658+DWheeler27@users.noreply.github.com>
2019-02-25 21:43:44 -0700

    Update RunGame.java

--- RunGame.java
+++ RunGame.java
@@ -6,10 +6,19 @@ public class RunGame(){
     Board b = new Board();
     Scanner input = new Scanner(System.in);
     
+    System.out.println("Welcome to our adventure game");
+		System.out.println("Please enter 1 to start");
+		Scanner startIn= new Scanner(System.in);
+		int startNumber= startIn.nextInt();
+		if(startNumber==1) {
+			init();
+		}
+		startIn.close();
+    
     while (true){
       // essentially: keeps checking for input, calls movement methods (b.moveLeft(), b.moveRight(), etc.) accordingly...
       // basically manages all user interaction stuff here.
-      Char command = input.nextChar();
+      Char command = input.nextLine();
       
       switch (command) {
         case 'q':  b.moveUpLeft();

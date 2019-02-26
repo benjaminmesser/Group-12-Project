5e22cb9868f55c615799a1b66e165b8fe49286f8
benjaminmesser <47367888+benjaminmesser@users.noreply.github.com>
2019-02-25 23:14:54 -0700

    Update Board.java
    Added getMap(), moveUpRight(), and moveUpLeft().

--- Board.java
+++ Board.java
@@ -28,11 +28,31 @@ public class Board{
     }
     updateMap();
   }
+  public void moveUpLeft(){
+    // checks to see if space is free above and to the left of the player's current position; if so, changes xPos accordingly.
+    if (p.getXPos() - 1 > 0 && p.getYPos() + 1 <= this.map[0].length){
+      if (map[p.getXPos() - 1][p.getYPos() + 1] == ' '){
+        p.setXPos(p.getXPos() - 1);
+        p.setYPos(p.getXPos() + 1);
+      }
+    }
+    updateMap();
+  }
   public void moveRight(){
     // checks to see if space is free to the right of the player's current position; if so, changes xPos accordingly.
     if (p.getXPos() + 1 <= this.map[0].length){
       if (map[p.getXPos() + 1][p.getYPos()] == ' '){
         p.setXPos(p.getXPos() + 1);
+      }
+    }
+    updateMap();
+  }
+  public void moveUpRight(){
+    // checks to see if space is free above and to the right of the player's current position; if so, changes xPos accordingly.
+    if (p.getXPos() + 1 <= this.map[0].length && p.getYPos() + 1 <= this.map[0].length){
+      if (map[p.getXPos() + 1][p.getYPos() + 1] == ' '){
+        p.setXPos(p.getXPos() + 1);
+        p.setYPos(p.getXPos() + 1);
       }
     }
     updateMap();
@@ -47,13 +67,22 @@ public class Board{
     updateMap();
   }
   public void fall(){
+    // checks to see if space is available below the player's current position; if so, changes yPos accordingly.
     if (p.getYPos() - 1 > 0){
       if (map[p.getXPos()][p.getYPos() - 1] == ' ')
         p.setYPos(p.getYPos() - 1);
       }
     }
-    // checks to see if space is available below the player's current position; if so, changes yPos accordingly.
     updateMap();
   }
-  
+  public char[][] getMap(){
+    //returns a copy of the map.
+    char[][] mapClone = new char[map.length][map[0].length];
+    for (int i = 0; i < map.length; i++){
+      for (int j = 0; j < map[0].length; j++){
+        mapClone[i][j] = map[i][j];
+      }
+    }
+    return mapClone;
+  }
 }

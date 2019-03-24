686e0bffabd25d2e961a2114b96966aa846bdd86
YifanChen1 <47368444+YifanChen1@users.noreply.github.com>
2019-03-24 14:37:41 -0600

    Add files via upload
    TestFiles + entity update

--- Entity.java
+++ Entity.java
@@ -1,14 +1,39 @@
 public abstract class Entity {
-  private int xPos;
-  private int yPos;
+  private int xPos = 1;
+  private int yPos = 1;
 
   public Entity(int x, int y) {
-    setXPos(x);
-    setYPos(y);
+    if (x > 0){
+      setXPos(x);
+    }
+    else{
+      setXPos(1);
+    }
+
+    if (y > 0){
+      setYPos(y);
+    }
+    else{
+      setYPos(1);
+    }
+  }
+  public Entity(Entity e){
+    if (e.xPos > 0){
+      this.xPos = e.xPos;
+    }
+    if (e.yPos > 0){
+      this.yPos = e.yPos;
+    }
   }
 
+
   public void setXPos(int x) {
-    this.xPos = x;
+    if(x > 0){
+      this.xPos = x;
+    }
+    else{
+      this.xPos = 1;
+    }
   }
 
   public int getXPos() {
@@ -16,7 +41,12 @@ public abstract class Entity {
   }
 
   public void setYPos(int y) {
+    if(y > 0){
       this.yPos = y;
+    }
+    else{
+      this.yPos = 1;
+    }
   }
 
   public int getYPos() {

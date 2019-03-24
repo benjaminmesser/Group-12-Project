5089448ed34a3ac42e66f776030535a9fbab1628
YifanChen1 <47368444+YifanChen1@users.noreply.github.com>
2019-03-24 14:38:31 -0600

    Add files via upload

--- Entity.java
+++ Entity.java
@@ -3,10 +3,19 @@ public abstract class Entity {
   private int yPos = 1;
 
   public Entity(int x, int y) {
-    if (x >= 0)
-    setXPos(x);
-    if (y >= 0)
-    setYPos(y);
+    if (x > 0){
+      setXPos(x);
+    }
+    else{
+      setXPos(1);
+    }
+
+    if (y > 0){
+      setYPos(y);
+    }
+    else{
+      setYPos(1);
+    }
   }
   public Entity(Entity e){
     if (e.xPos > 0){
@@ -17,9 +26,14 @@ public abstract class Entity {
     }
   }
 
+
   public void setXPos(int x) {
-    if(x > 0)
-    this.xPos = x;
+    if(x > 0){
+      this.xPos = x;
+    }
+    else{
+      this.xPos = 1;
+    }
   }
 
   public int getXPos() {
@@ -27,8 +41,12 @@ public abstract class Entity {
   }
 
   public void setYPos(int y) {
-    if (y > 0)
-    this.yPos = y;
+    if(y > 0){
+      this.yPos = y;
+    }
+    else{
+      this.yPos = 1;
+    }
   }
 
   public int getYPos() {

12f965a4fb08c772475fe7d1f4df049e53f26db3
DWheeler27 <dwheeler1127@gmail.com>
2019-03-24 20:34:35 -0600

    Update BadMario.java
    added a bunch of imports, still doesn't compile

--- BadMario.java
+++ BadMario.java
@@ -1,3 +1,16 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.Group;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
+import javafx.scene.paint.Color;
+import javafx.animation.AnimationTimer;
+import javafx.scene.input.KeyEvent;
+import java.util.ArrayList;
+import java.util.Iterator;
+
 public class BadMario extends Application {
   public static void main(String[] args) {
     launch(args);
@@ -38,5 +51,7 @@ public class BadMario extends Application {
 
 
     });
+
+    stage.show();
   }
 }

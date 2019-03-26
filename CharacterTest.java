58cb2612098b2b1ab828311746bbfe8d2bd0a3a9
YifanChen1 <47368444+YifanChen1@users.noreply.github.com>
2019-03-26 12:26:36 -0600

    Update CharacterTest.java

--- CharacterTest.java
+++ CharacterTest.java
@@ -16,8 +16,8 @@ public class CharacterTest{
         @Test
         public void test_invalid_Constructor(){
             Character b = new Character(-1,-1);
-            assertEquals("Expected x position to be 1", 1, b.getXPos(), 0.00001);
-            assertEquals("Expected y position to be 1", 1, b.getYPos(), 0.00001);
+            assertEquals("Expected x position to be 0", 0, b.getXPos(), 0.00001);
+            assertEquals("Expected y position to be 0", 0, b.getYPos(), 0.00001);
         }
 
         @Test
@@ -25,8 +25,8 @@ public class CharacterTest{
             Character e1 = new Character(5,10);
             Character e2  = new Character(e1);
 
-            assertEquals("Expected x position to be 1", 5, e2.getXPos(), 0.00001);
-            assertEquals("Expected y position to be 1", 10, e2.getYPos(), 0.00001);
+            assertEquals("Expected x position to be 5", 5, e2.getXPos(), 0.00001);
+            assertEquals("Expected y position to be 10", 10, e2.getYPos(), 0.00001);
         }
 
         @Test
@@ -47,14 +47,14 @@ public class CharacterTest{
         public void test_invalid_X(){
             Character b = new Character(5,5);
             b.setXPos(-1);
-            assertEquals("Expected x position to be 1", 1, b.getXPos(), 0.00001);
+            assertEquals("Expected x position to be 0", 0, b.getXPos(), 0.00001);
         }
 
         @Test
         public void test_invalid_Y(){
             Character b = new Character(5,5);
             b.setYPos(-1);
-            assertEquals("Expected y position to be 1", 1, b.getYPos(), 0.00001);
+            assertEquals("Expected y position to be 0", 0, b.getYPos(), 0.00001);
         }
             
     } 

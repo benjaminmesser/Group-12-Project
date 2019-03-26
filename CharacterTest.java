7927cd060584c31ced632d598f86d3986b0f4629
YifanChen1 <47368444+YifanChen1@users.noreply.github.com>
2019-03-26 13:22:12 -0600

    Update CharacterTest.java

--- CharacterTest.java
+++ CharacterTest.java
@@ -20,14 +20,6 @@ public class CharacterTest{
             assertEquals("Expected y position to be 0", 0, b.getYPos(), 0.00001);
         }
 
-        @Test
-        public void test_copy_constuctor(){
-            Character e1 = new Character(5,10);
-            Character e2  = new Character(e1);
-
-            assertEquals("Expected x position to be 5", 5, e2.getXPos(), 0.00001);
-            assertEquals("Expected y position to be 10", 10, e2.getYPos(), 0.00001);
-        }
 
         @Test
         public void test_Set_X(){
